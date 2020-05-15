87aa1df71f7fe41be1b51cc2f2c4cff02f6bfd12
hucheng <hucheng@gmail.com>
2020-05-15 15:57:08 +0800

    SpringCloud Security OAuth2的使用

---
+++ base-resource/src/main/java/com/hucheng/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.hucheng.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author HuChan
+ */
+@RestController
+public class TestController {
+    //测试接口
+    @RequestMapping("r1")
+    @PreAuthorize("hasAnyAuthority('admin')")
+    public String admin() {
+        return "访问资源成功";
+    }
+}
---
+++ base-uua/src/main/java/com/hucheng/config/TokenConfig.java
@@ -0,0 +1,18 @@
+package com.hucheng.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+/**
+ * @author HuChan
+ */
+@Configuration
+public class TokenConfig {
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+}
