c7cddf04be88c95f4cef980edb068398fac9c114
hucheng <hucheng@gmail.com>
2020-05-18 09:55:07 +0800

    SpringCloud Security OAuth2的使用

--- base-uua/src/main/java/com/hucheng/bean/MyUserDetails.java
+++ base-uua/src/main/java/com/hucheng/bean/MyUserDetails.java
@@ -1,8 +1,6 @@
 package com.hucheng.bean;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,13 +12,46 @@ import java.util.stream.Stream;
 /**
  * @author HuChan
  */
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class MyUserDetails implements UserDetails {
     private String username;
     private String password;
     private String perms;
+
+    public MyUserDetails() {
+    }
+
+    public MyUserDetails(String username, String password, String perms) {
+        this.username = username;
+        this.password = password;
+        this.perms = perms;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPerms() {
+        return perms;
+    }
+
+    public void setPerms(String perms) {
+        this.perms = perms;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
